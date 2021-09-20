9b31ba73feab4e4e717501efb0295afe7e982c0a
krbalaga <kckckrishnachaITANYA7@gmail.com>
2021-09-20 23:09:34 +0530

    updated export

--- installMongodb.sh
+++ installMongodb.sh
@@ -9,12 +9,10 @@ read -p "=== Continue to install (y/n)? " CONT
 if [ "$CONT" = "y" ]; then
 USER_PASS=`openssl rand -base64 12 | tr -d "=+/"`
 helm install mongo bitnami/mongodb --set global.storageClass=ibmc-block-gold,auth.password=$USER_PASS,auth.username=guestbook-admin,auth.database=guestbook,containerSecurityContext.enabled=false,podSecurityContext.enabled=false -n mongo
-export MONGODB_ROOT_PASSWORD=$(kubectl get secret --namespace mongo mongo-mongodb -o jsonpath="{.data.mongodb-root-password}" | base64 --decode)
-export MONGODB_PASSWORD=$(kubectl get secret --namespace mongo mongo-mongodb -o jsonpath="{.data.mongodb-password}" | base64 --decode)
 oc get all -n mongo
 oc get pvc -n mongo
 echo "=== Please wait till PV is created and mongo pod is up, it usually takes upto 3 minutes ==="
-echo "=== Use 'oc get pods -n mongo' to check if the pod is status === "
+echo "=== Use 'oc get pods -n mongo' to check if the pod is up === "
 else
   echo "=== Script stopped ===";
 fi
