3869072244b28c862415f12057344305ee60cc5e
krbalaga <kckckrishnachaITANYA7@gmail.com>
2021-09-20 22:37:08 +0530

    updated auth.username set

--- installMongodb.sh
+++ installMongodb.sh
@@ -8,11 +8,13 @@ cat mongdb-install-dryrun.yaml
 read -p "=== Continue to install (y/n)? " CONT
 if [ "$CONT" = "y" ]; then
 USER_PASS=`openssl rand -base64 12 | tr -d "=+/"`
-helm install mongo bitnami/mongodb --set global.storageClass=ibmc-block-gold,auth.password=$USER_PASS,auth.username=guestbookadmin,auth.database=guestbook,containerSecurityContext.enabled=false,podSecurityContext.enabled=false -n mongo
+helm install mongo bitnami/mongodb --set global.storageClass=ibmc-block-gold,auth.password=$USER_PASS,auth.username=guestbook-admin,auth.database=guestbook,containerSecurityContext.enabled=false,podSecurityContext.enabled=false -n mongo
 export MONGODB_PASSWORD=$(kubectl get secret --namespace mongo mongo-mongodb -o jsonpath="{.data.mongodb-password}" | base64 --decode)
-export MONGODB_ROOT_PASSWORD=$(kubectl get secret --namespace mongo mongo-mongodb -o jsonpath="{.data.mongodb-root-password}" | base64 --decode)
+export MONGODB_PASSWORD=$(kubectl get secret --namespace mongo mongo-mongodb -o jsonpath="{.data.mongodb-password}" | base64 --decode)
 oc get all -n mongo
 oc get pvc -n mongo
+echo "=== Please wait till PV is created and mongo pod is up, it usually takes upto 3 minutes ==="
+echo "=== Use `oc pods -n mongo` to check if the pod is status === "
 else
   echo "=== Script stopped ===";
 fi
