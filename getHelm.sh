321db0d3ed486bd561e63e80a2680941cd653fed
krbalaga <kckckrishnachaITANYA7@gmail.com>
2021-10-05 15:07:59 +0530

    updated content for secrets demo

--- getHelm.sh
+++
@@ -1,5 +0,0 @@
-cd $HOME
-wget https://get.helm.sh/helm-v3.2.0-linux-amd64.tar.gz
-tar -zxvf helm-v3.2.0-linux-amd64.tar.gz
-echo 'export PATH=$HOME/linux-amd64:$PATH' > $HOME/.bash_profile
-source $HOME/.bash_profile
